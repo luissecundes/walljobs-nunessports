0b5aacb42c4b5bd095eb5b20e62e8529e9083099
Luis <luissecundes@gmail.com>
2024-01-04 11:01:10 -0300

    Criação do backend em Java

---
+++ backend/nunessports/src/main/java/com/walljobs/nunessports/NunessportsApplication.java
@@ -0,0 +1,13 @@
+package com.walljobs.nunessports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunessportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunessportsApplication.class, args);
+	}
+
+}
---
+++ backend/nunessports/src/test/java/com/walljobs/nunessports/NunessportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walljobs.nunessports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunessportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
