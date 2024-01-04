60c60afcba7783854260018271f7376c8f848ffa
Luis <luissecundes@gmail.com>
2024-01-04 11:23:17 -0300

    Criado repositório

---
+++ backend/nunessports/src/main/java/com/walljobs/nunessports/repositories/NunesSportsProductRepository.java
@@ -0,0 +1,9 @@
+package com.walljobs.nunessports.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.walljobs.nunessports.entities.NunesSportsProduct;
+
+public interface NunesSportsProductRepository extends JpaRepository<NunesSportsProduct, Long> {
+
+}
