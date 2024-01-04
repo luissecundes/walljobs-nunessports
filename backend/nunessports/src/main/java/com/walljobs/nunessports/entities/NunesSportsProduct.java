a4d45c97aea38723df9206289843e742f0685d79
Luis <luissecundes@gmail.com>
2024-01-04 11:08:58 -0300

    Feito ORM da aplicação

---
+++ backend/nunessports/src/main/java/com/walljobs/nunessports/entities/NunesSportsProduct.java
@@ -0,0 +1,111 @@
+package com.walljobs.nunessports.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_product")
+public class NunesSportsProduct {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;	
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	private double price;
+	
+	
+	public NunesSportsProduct() {
+		
+	}
+
+	
+
+	public NunesSportsProduct(Long id, String name, String description, double price) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NunesSportsProduct other = (NunesSportsProduct) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
