558a9ee9cbf46284e6a99bc01e62f44c2197decd
Luis <luissecundes@gmail.com>
2024-01-05 12:23:00 -0300

    Finalizada a página Home e feito a estilização

--- frontend/nunes-sports-frontend/src/components/pages/Home.js
+++ frontend/nunes-sports-frontend/src/components/pages/Home.js
@@ -1,8 +1,15 @@
-
+import styles from './Home.module.css'
+import savings from '../../img/savings.png'
+import LinkButton from '../layouts/LinkButton';
 
 function Home() {
   return (
-  <div><h1>Home</h1></div>
+    <section className={styles.home_container}>
+      <h1>Bem-vindo à <span>Nunes Sports</span></h1>
+      <p>Gerencie aqui os seus produtos!</p>
+      <LinkButton to="/addproducts" text="Adicionar Produto" />
+      <img src={savings} alt='Nunes Sports'></img>
+    </section>
   );
 }
 
