04e0813070d58be0bf0c02155c4ab771fc731b6a
Luis <luissecundes@gmail.com>
2024-01-05 14:15:27 -0300

    Página de Products já está recebendo o JSON

--- frontend/nunes-sports-frontend/src/components/pages/Products.js
+++ frontend/nunes-sports-frontend/src/components/pages/Products.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
 function Products() {
-  return  (
-    <div><h1>Produtos</h1></div>
-    )
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    // Função para buscar a lista de produtos do servidor
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/products');
+        const data = await response.json();
+        setProductList(data);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+
+    // Chame a função de busca ao montar o componente
+    fetchProducts();
+  }, []); // O segundo parâmetro [] assegura que o useEffect só será chamado na montagem inicial
+
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <ul>
+        {productList.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> - {product.description} - R${product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
--- frontend/nunes-sports-frontend/src/components/products/ProductForm.js
+++ frontend/nunes-sports-frontend/src/components/products/ProductForm.js
@@ -1,15 +1,34 @@
+import { useState, useEffect } from 'react'
+
 import styles from './ProductForm.module.css'
 import Input from '../form/Input'
 import SubmitButton from '../form/SubmitButton'
 
 
-function Productform({btnText}) {
+function Productform({ btnText }) {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/products", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProducts(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
-    <form className={styles.form}>      
-      <Input type="text" text="Nome do Produto" placeholder="Insira o nome do produto"/>
-      <Input type="text" text="Descrição do Produto" placeholder="Insira a descrição do produto"/>
-      <Input type="number" text="Preço do Produto" placeholder="Insira o preço do produto"/>
-      <SubmitButton text={btnText}/>
+    <form className={styles.form}>
+      <Input type="text" text="Nome do Produto" placeholder="Insira o nome do produto" />
+      <Input type="text" text="Descrição do Produto" placeholder="Insira a descrição do produto" />
+      <Input type="number" text="Preço do Produto" placeholder="Insira o preço do produto" />
+      <SubmitButton text={btnText} />
     </form>
   )
 }
