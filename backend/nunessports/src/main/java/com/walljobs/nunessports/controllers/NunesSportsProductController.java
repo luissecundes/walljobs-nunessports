925e77ddfa6c3dec2cb5df16a8405caacc2ebefc
Luis <luissecundes@gmail.com>
2024-01-08 06:28:30 -0300

    Feito tratamento de exceções no backend

--- backend/nunessports/src/main/java/com/walljobs/nunessports/controllers/NunesSportsProductController.java
+++ backend/nunessports/src/main/java/com/walljobs/nunessports/controllers/NunesSportsProductController.java
@@ -16,48 +16,68 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.walljobs.nunessports.entities.NunesSportsProduct;
+import com.walljobs.nunessports.exceptions.NunesSportsException;
 import com.walljobs.nunessports.services.NunesSportsProductService;
 
 @RestController
-@CrossOrigin (origins = "*")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/products")
 public class NunesSportsProductController {
 
-	@Autowired
-	private NunesSportsProductService nunesSportsProductService;
+    @Autowired
+    private NunesSportsProductService nunesSportsProductService;
 
-	@GetMapping
-	public List<NunesSportsProduct> findall() {
-		var result = nunesSportsProductService.findAll();
-		return result;
-	}
-
-	@GetMapping(value = "/{id}")
-	public NunesSportsProduct findById(@PathVariable Long id) {
-		NunesSportsProduct result = nunesSportsProductService.findById(id);
-		return result;
-	}
-
-	@PostMapping
-    public ResponseEntity<String> save(@RequestBody NunesSportsProduct product) {
-        nunesSportsProductService.save(product);
-        return new ResponseEntity<>("Produto salvo com sucesso!", HttpStatus.CREATED);
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        try {
+            List<NunesSportsProduct> result = nunesSportsProductService.findAll();
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (NunesSportsException.ProductsNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
-	 @PutMapping(value = "/{id}")
-	    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody NunesSportsProduct updatedProduct) {
-	        NunesSportsProduct updated = nunesSportsProductService.update(id, updatedProduct);
-	        if (updated != null) {
-	            return new ResponseEntity<>("Produto atualizado com sucesso!", HttpStatus.OK);
-	        } else {
-	            return new ResponseEntity<>("Produto não encontrado.", HttpStatus.NOT_FOUND);
-	        }
-	    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            NunesSportsProduct result = nunesSportsProductService.findById(id);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (NunesSportsException.ProductsNotFoundIdException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 
-	 @DeleteMapping(value = "/{id}")
-	    public ResponseEntity<String> delete(@PathVariable Long id) {
-	        nunesSportsProductService.delete(id);
-	        return new ResponseEntity<>("Produto excluído com sucesso!", HttpStatus.OK);
-	    }
+    @PostMapping
+    public ResponseEntity<String> save(@RequestBody NunesSportsProduct product) {
+        try {
+            nunesSportsProductService.save(product);
+            return new ResponseEntity<>("Produto salvo com sucesso!", HttpStatus.CREATED);
+        } catch (NunesSportsException.ProductNotSavedException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody NunesSportsProduct updatedProduct) {
+        try {
+            NunesSportsProduct updated = nunesSportsProductService.update(id, updatedProduct);
+            if (updated != null) {
+                return new ResponseEntity<>("Produto atualizado com sucesso!", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Produto não encontrado.", HttpStatus.NOT_FOUND);
+            }
+        } catch (NunesSportsException.ProductNotUpdatedException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        try {
+            nunesSportsProductService.delete(id);
+            return new ResponseEntity<>("Produto excluído com sucesso!", HttpStatus.OK);
+        } catch (NunesSportsException.ProductNotDeletedException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ backend/nunessports/src/main/java/com/walljobs/nunessports/exceptions/NunesSportsException.java
@@ -0,0 +1,45 @@
+package com.walljobs.nunessports.exceptions;
+
+public class NunesSportsException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public NunesSportsException(String message) {
+		super(message);
+	}
+
+	public static class ProductsNotFoundException extends NunesSportsException {
+		private static final long serialVersionUID = 1L;
+
+		public ProductsNotFoundException() {
+			super("Não foi possível encontrar a lista de produtos.");
+		}
+	}
+	public static class ProductsNotFoundIdException extends NunesSportsException {
+		private static final long serialVersionUID = 1L;
+
+		public ProductsNotFoundIdException() {
+			super("Não foi possível encontrar a lista de produtos.");
+		}
+	}
+
+	public static class ProductNotSavedException extends NunesSportsException {
+		private static final long serialVersionUID = 1L;
+		public ProductNotSavedException() {
+			super("Não foi possível salvar o produto na base de dados.");
+		}
+	}
+
+	public static class ProductNotUpdatedException extends NunesSportsException {
+		private static final long serialVersionUID = 1L;
+		public ProductNotUpdatedException(Long productId) {
+			super("Não foi possível atualizar o produto com ID " + productId + ".");
+		}
+	}
+
+	public static class ProductNotDeletedException extends NunesSportsException {
+		private static final long serialVersionUID = 1L;
+		public ProductNotDeletedException(Long productId) {
+			super("Não foi possível excluir o produto com ID: " + productId + ".");
+		}
+	}
+}
