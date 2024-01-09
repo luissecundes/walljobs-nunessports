2a40ef8183781a13c047ae789e23c8aa4f3a5df4
Luis <luissecundes@gmail.com>
2024-01-09 12:02:20 -0300

    Feitas alterações estéticas na tabela.

--- frontend/nunes-sports-frontend/src/components/buttons/EditButton.js
+++ frontend/nunes-sports-frontend/src/components/buttons/EditButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
+import styles from './EditButton.module.css'; 
+
 
 function EditButton({ onClick }) {
   return (
-    <button onClick={onClick} className="edit-button">
+    <button onClick={onClick} className={styles.editButton}>
       <EditIcon />
     </button>
   );
